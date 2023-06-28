bffb02e73adeea37c67b58de249426c20f1db630
treyt3n <streyten58@gmail.com>
2023-06-28 12:59:33 -0400

    yes

--- boltons/__init__.py
+++ boltons/__init__.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 
- from strutils import *
- from excutils import *
- from iterutils import *
- from funcutils import *
- from queueutils import *
- from dictutils import *
- from setutils import *
- from listutils import *
- from statsutils import *
+from . import (
+    strutils,
+    excutils,
+    iterutils,
+    funcutils,
+    queueutils,
+    dictutils,
+    setutils,
+    listutils,
+    statsutils,
 
- from compat import make_sentinel, IS_PY2, IS_PY3
+from compat import make_sentinel, IS_PY2, IS_PY3
